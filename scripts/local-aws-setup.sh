7fbe799de9d9d94c1492f712bf154b9341451d80
LostButton <71197188+LostButton@users.noreply.github.com>
2022-11-22 08:35:44 -0500

    lab 9 steps 6-7

--- scripts/local-aws-setup.sh
+++ scripts/local-aws-setup.sh
@@ -15,25 +15,25 @@ export AWS_SECRET_ACCESS_KEY=test
 echo "AWS_SESSION_TOKEN=test"
 export AWS_SESSION_TOKEN=test
 
-export AWS_DEFAULT_REGION=us-east-1
 echo "AWS_DEFAULT_REGION=us-east-1"
+export AWS_DEFAULT_REGION=us-east-1
 
 # Wait for LocalStack to be ready, by inspecting the response from /health, see:
 # https://github.com/localstack/localstack/issues/4904#issuecomment-966315170
 echo 'Waiting for LocalStack S3...'
-until (curl --silent http://localhost:4566/health | grep "\"s3\": \"\(running\|available\)\"" > /dev/null); do
+until (curl --silent http://127.0.0.1:4566/health | grep "\"s3\": \"\(running\|available\)\"" > /dev/null);do 
     sleep 5
 done
 echo 'LocalStack S3 Ready'
 
 # Create our S3 bucket with LocalStack
 echo "Creating LocalStack S3 bucket: fragments"
-aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket fragments
+aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket fragments
 
 # Setup DynamoDB Table with dynamodb-local, see:
 # https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/getting-started-step-1.html
 echo "Creating DynamoDB-Local DynamoDB table: fragments"
-aws --endpoint-url=http://localhost:8000 \
+aws --endpoint-url=http://127.0.0.1:8000 \
 dynamodb create-table \
     --table-name fragments \
     --attribute-definitions \
@@ -47,4 +47,4 @@ dynamodb create-table \
 
 # Wait until the Fragments table exists in dynamodb-local, so we can use it, see:
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/dynamodb/wait/table-exists.html
-aws --endpoint-url=http://localhost:8000 dynamodb wait table-exists --table-name fragments
+aws --endpoint-url=http://127.0.0.1:8000 dynamodb wait table-exists --table-name fragments
--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -1,7 +1,9 @@
 const s3Client = require('./s3Client');
 const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 
+// XXX: temporary use of memory-db until we add DynamoDB
 const MemoryDB = require('../memory/memory-db');
+
 const logger = require('../../../logger');
 // Create two in-memory databases: one for fragment metadata and the other for raw data
 
