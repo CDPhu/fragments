410289b2a406d9f4f37928e18d455f1a6f3710e3
LostButton <71197188+LostButton@users.noreply.github.com>
2022-11-22 19:16:15 -0500

    reverting post

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,23 +1,31 @@
 const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const logger = require('../../logger');
+require('dotenv').config();
 
 module.exports = async (req, res) => {
   // TODO: this is just a placeholder to get something working...
   let fragment;
   const api = process.env.API_URL;
-  try {
+  if (Buffer.isBuffer(req.body) === true) {
     fragment = new Fragment({
       ownerId: req.user,
       type: req.get('Content-type'),
       size: req.body.length,
     });
-    await fragment.save();
-    await fragment.setData(req.body);
-    res.location(`${api}/v1/fragments/${fragment.id}`);
-    res.status(201).json(createSuccessResponse({ fragment }));
-    logger.info({ fragment: fragment }, `successfully posted fragment `);
-  } catch (err) {
+    try {
+      await fragment.save();
+      await fragment.setData(req.body);
+      res.location(`${api}/v1/fragments/${fragment.id}`);
+      res.status(201).json(createSuccessResponse({ fragment }));
+      logger.info({ fragment: fragment }, `successfully posted fragment`);
+    } catch (err) {
+      res.status(415).json(415, 'unable to post fragment');
+      logger.info({ fragment: fragment });
+    }
+  } else {
     res.status(415).json(createErrorResponse(415, 'not supported type'));
+    logger.info({ fragment: fragment });
+    logger.info(`posting fragment was unsuccessful`);
   }
 };
