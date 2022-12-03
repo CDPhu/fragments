66c5b78ddefca765aaf6ab261849c34db95c50e5
LostButton <71197188+LostButton@users.noreply.github.com>
2022-12-03 16:31:54 -0500

    modified text conversion

--- src/model/fragment.js
+++ src/model/fragment.js
@@ -218,19 +218,35 @@ class Fragment {
    * @param {string} value a convert type value
    * @returns result
    */
+  // async txtConvert(value) {
+  //   var result, fragData;
+  //   fragData = await this.getData();
+  //   if (value == 'html' && this.type != 'text/html') {
+  //     if (this.type == 'text/markdown') {
+  //       result = md.render(fragData.toString());
+  //     }
+  //   } else if (this.type == 'application/json' && value == 'plain') {
+  //     result = JSON.parse(fragData);
+  //   } else if (this.type == 'text/html' && value == 'plain') {
+  //     result = fragData;
+  //   } else if (value != 'md' && this.type != 'text/plain') {
+  //     result = fragData;
+  //   }
+  //   return result;
+  // }
   async txtConvert(value) {
     var result, fragData;
     fragData = await this.getData();
-    if (value == 'html' && this.type != 'text/html') {
-      if (this.type == 'text/markdown') {
+    if (value == 'plain') {
+      if (this.type == 'application/json') {
+        result = JSON.parse(fragData);
+      } else {
+        result = fragData;
+      }
+    } else if (value == 'html') {
+      if (this.type.endsWith('markdown')) {
         result = md.render(fragData.toString());
       }
-    } else if (this.type == 'application/json' && value == 'text') {
-      result = JSON.parse(fragData);
-    } else if (value == 'md' && this.type == 'text/markdown') {
-      result = fragData;
-    } else if (value != 'md') {
-      result = fragData;
     }
     return result;
   }
@@ -256,6 +272,8 @@ class Fragment {
       ext = 'plain';
     } else if (value == 'jpg') {
       ext = 'jpeg';
+    } else if (value == 'md') {
+      ext = 'markdown';
     } else {
       ext = value;
     }
